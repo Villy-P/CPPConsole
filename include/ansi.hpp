f3c2a3702e1fce862a0905d03ba0c10cedf831f5
Villy-P <valeriuspetrini@gmail.com>
2024-07-14 22:18:10 -0500

    Add `ansi.hpp`

---
+++ include/ansi.hpp
@@ -0,0 +1,13 @@
+#ifndef __CPP_CONSOLE_ANSI_HPP__
+#define __CPP_CONSOLE_ANSI_HPP__
+
+#include "console.hpp"
+
+/**
+ * @brief Namespace Containing all console commands
+ */
+namespace console {
+
+}
+
+#endif
