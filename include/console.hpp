de636a295a6a12120837382fc1394073c261e2a1
Villy-P <valeriuspetrini@gmail.com>
2024-07-14 22:07:04 -0500

    Add `print`

--- include/console.hpp
+++ include/console.hpp
@@ -1,11 +1,46 @@
 #ifndef __CPP_CONSOLE_HPP__
 #define __CPP_CONSOLE_HPP__
 
+#include "template.hpp"
+
 /**
  * @brief Namespace Containing all console commands
  */
 namespace console {
-
+    /**
+     * @brief Prints out a value
+     * 
+     * @tparam T Type of the value
+     * @param value The item to print
+     */
+    template<typename T>
+    void print(const T& value) { 
+        if constexpr (console::is_map<T>::is) {
+            print("{ ");
+            for (auto it = value.begin(); it != value.end(); ++it) {
+                print("{");
+                print(it->first);
+                print(", ");
+                print(it->second);
+                print("}");
+                if (it != std::prev(value.end()))
+                    print(", ");
+            }
+            print(" }");
+        } else if constexpr (console::iterable<T>::is) {
+            print("{ ");
+            std::size_t count = 0;
+            for (const auto& element : value) {
+                print(element);
+                if (count != value.size() - 1)
+                    print(", ");
+                count++;
+            }
+            print(" }");
+        } else {
+            std::cout << value; 
+        }
+    }
 }
 
 #endif
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <map>
+#include <vector>
+
+#include "console.hpp"
 
 int main() {
-    std::cout << "Hello" << std::endl;
+    std::vector<int> vec = {0, 1, 2, 4, 8, 16};
+    std::map<int, int> m = {
+        {0, 0},
+        {1, 1},
+        {2, 4},
+        {3, 9},
+        {4, 16}
+    };
+
+    console::print("Wow!");
+    console::print(vec);
+    console::print(m);
     return 0;
 }
