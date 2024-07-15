e9c10f81ee7b1ba4667ac8d96f0473b8ca951e9c
Villy-P <valeriuspetrini@gmail.com>
2024-07-14 22:32:44 -0500

    Add `data.hpp`

---
+++ include/data.hpp
@@ -0,0 +1,13 @@
+#ifndef __CPP_CONSOLE_CURSOR_HPP__
+#define __CPP_CONSOLE_CURSOR_HPP__
+
+#include "console.hpp"
+
+/**
+ * @brief Namespace Containing all console commands
+ */
+namespace console {
+
+}
+
+#endif
