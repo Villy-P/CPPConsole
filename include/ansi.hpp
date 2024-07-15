6c97416687be35ec63f1e2c8edd0305cab58aa5b
Villy-P <valeriuspetrini@gmail.com>
2024-07-14 22:18:56 -0500

    Add `isANSIEnabled`

--- include/ansi.hpp
+++ include/ansi.hpp
@@ -3,11 +3,34 @@
 
 #include "console.hpp"
 
+#ifdef __WIN32
+#include <Windows.h>
+#else
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#endif
+
 /**
  * @brief Namespace Containing all console commands
  */
 namespace console {
-
+    /**
+     * @brief Checks if ANSI Codes are enable in windows.
+     * 
+     * @return True, if enabled, false if not. Always returns true on Linux systems
+    */
+    bool isANSIEnabled() {
+        #ifdef __WIN32
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        DWORD consoleMode;
+        GetConsoleMode(hConsole, &consoleMode);
+        return consoleMode & ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        #else
+        return true;
+        #endif
+    }
 }
 
 #endif
