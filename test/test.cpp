4456f3140b020baeee7f6aecfd05c1ae5244a1ab
Villy-P <valeriuspetrini@gmail.com>
2024-12-27 15:31:16 -0600

    feat(test): enhance console test with additional output and structure printing

--- test/test.cpp
+++ test/test.cpp
@@ -9,6 +9,14 @@
 #include "graphics.hpp"
 
 int main() {
+    console::println("Testing C++ Console. Press ENTER to begin.");
+    std::cin.get();
+
+    console::eraseEntireScreen();
+    console::moveCursorHome();
+    console::println("Console Has Been Cleared");
+    console::println("Testing Printing Data Structures");
+
     std::vector<int> vec = {0, 1, 2, 4, 8, 16};
     std::map<int, int> m = {
         {0, 0},
@@ -19,28 +27,33 @@ int main() {
     };
     int arr[] = {0, 1, 2, 3, 4};
 
-    console::println("Wow!");
+    console::print("Vector: ");
     console::println(vec);
+    console::print("Map: ");
     console::println(m);
+    console::print("Array: ");
     console::println(arr);
 
     console::println();
+    console::println("Testing PrintAll");
+    console::printAll("Hello", "World", "From", "C++", "Console!\n");
+    console::printAll("Wow!", vec, m, arr, "\n");
     console::printlnAll("Wow!", vec, m, arr);
 
-    console::println(console::bold("Bold Text!"));
-    console::println(console::faint("Faint Text!"));
-    console::println(console::italic("Italic Text!"));
-    console::println(console::bold(console::faint(console::italic("Bold, Faint, Italic Text!"))));
-    console::println(console::underline("Underlined Text!"));
-    console::println(console::blink("Blinking Text!"));
-    console::println(console::inverse("Reversed Text!"));
-    console::println(console::hidden("Hidden Text!"));
-    console::println(console::strikethrough("Strikethrough Text!"));
-    console::println(console::bold(console::faint(console::italic(console::underline(console::blink(console::inverse(console::strikethrough("All Text Effects!"))))))));
+    // console::println(console::bold("Bold Text!"));
+    // console::println(console::faint("Faint Text!"));
+    // console::println(console::italic("Italic Text!"));
+    // console::println(console::bold(console::faint(console::italic("Bold, Faint, Italic Text!"))));
+    // console::println(console::underline("Underlined Text!"));
+    // console::println(console::blink("Blinking Text!"));
+    // console::println(console::inverse("Reversed Text!"));
+    // console::println(console::hidden("Hidden Text!"));
+    // console::println(console::strikethrough("Strikethrough Text!"));
+    // console::println(console::bold(console::faint(console::italic(console::underline(console::blink(console::inverse(console::strikethrough("All Text Effects!"))))))));
 
-    console::setBold(true);
-    console::println("Bold Text!");
-    console::setBold(false);
-    console::println("Normal Text!");
+    // console::setBold(true);
+    // console::println("Bold Text!");
+    // console::setBold(false);
+    // console::println("Normal Text!");
     return 0;
 }
