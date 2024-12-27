0c7e271f89a81f340ffe35403631087779290e8e
Villy-P <valeriuspetrini@gmail.com>
2024-12-27 16:51:41 -0600

    feat(console): add setColor functions for predefined, custom, and RGB colors

--- include/color.hpp
+++ include/color.hpp
@@ -88,6 +88,42 @@ namespace console {
             return "\033[" + std::to_string(foreground ? 38 : 48) + ";2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + str + "\033[0m";
         return str;
     }
+
+    /**
+     * @brief Sets the color of the text to a predefined color
+     * 
+     * @param color The color to set the text to
+     */
+    void setColor(Graphics color) {
+        if (isANSIEnabled())
+            std::cout << "\033[" << std::to_string(color) << "m";
+    }
+
+    /**
+     * @brief Sets the color of the text to a custom color, between 0 to 255
+     * 
+     * From 0 to 15, the colors are the standard ANSI colors
+     * From 16 to 231, the color uses the forumla `16 + (R × 36) + (G × 6) + B`
+     * From 232 to 255, the colors are grayscale
+     * 
+     * @param color Color to set the text to
+     * @param foreground Whether to set the foreground or background color (default is foreground)
+     */
+    void setColor(unsigned char color, bool foreground = true) {
+        if (isANSIEnabled())
+            std::cout << "\033[" << (foreground ? "38" : "48") << ";5;" << std::to_string(color) << "m";
+    }
+
+    /**
+     * @brief Sets the color of the text to a custom RGB color
+     * 
+     * @param color The color to set the text to
+     * @param foreground Whether to set the foreground or background color (default is foreground)
+     */
+    void setColor(unsigned char r, unsigned char g, unsigned char b, bool foreground = true) {
+        if (isANSIEnabled())
+            std::cout << "\033[" << (foreground ? "38" : "48") << ";2;" << std::to_string(r) << ";" << std::to_string(g) << ";" << std::to_string(b) << "m";
+    }
 }
 
 #endif
