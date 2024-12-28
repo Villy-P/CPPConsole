509ff73c21060f6ed3c576c1925ab37036369bf3
Villy-P <valeriuspetrini@gmail.com>
2024-12-27 21:40:30 -0600

    feat(console): add Dimensions structure for console dimensions representation

---
+++ include/dimensions.hpp
@@ -0,0 +1,17 @@
+#ifndef __CPP_CONSOLE_DIMENSIONS_HPP__
+#define __CPP_CONSOLE_DIMENSIONS_HPP__
+
+/**
+ * @brief Namespace Containing all console commands
+ */
+namespace console {
+    /**
+     * @brief A structure representing the dimensions of the console
+     */
+    typedef struct {
+        int rows;
+        int cols;
+    } Dimensions;
+}
+
+#endif
