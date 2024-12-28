052dd06c0e9d1bb6e1404773bfa16c3caa1aeda6
Villy-P <valeriuspetrini@gmail.com>
2024-12-27 21:51:28 -0600

    feat(console): move Graphics enum to graphics.hpp for better organization

--- include/graphics.hpp
+++ include/graphics.hpp
@@ -10,6 +10,55 @@
  */
 namespace console {
     /**
+     * @brief Enumeration of builtin colors for ANSI escape codes, along with other graphics modes
+     * 
+     */
+    enum Graphics {
+        BOLD = 1,
+        DIM,
+        ITALIC,
+        UNDERLINE,
+        BLINK,
+        INVERT,
+        HIDDEN,
+        STRIKETHROUGH,
+        BLACK = 30,
+        RED,
+        GREEN,
+        YELLOW,
+        BLUE,
+        MAGENTA,
+        CYAN,
+        WHITE,
+        DEFAULT = 39,
+        BG_BLACK = 40,
+        BG_RED,
+        BG_GREEN,
+        BG_YELLOW,
+        BG_BLUE,
+        BG_MAGENTA,
+        BG_CYAN,
+        BG_WHITE,
+        BG_DEFAULT = 49,
+        BRIGHT_BLACK = 90,
+        BRIGHT_RED,
+        BRIGHT_GREEN,
+        BRIGHT_YELLOW,
+        BRIGHT_BLUE,
+        BRIGHT_MAGENTA,
+        BRIGHT_CYAN,
+        BRIGHT_WHITE,
+        BG_BRIGHT_BLACK = 100,
+        BG_BRIGHT_RED,
+        BG_BRIGHT_GREEN,
+        BG_BRIGHT_YELLOW,
+        BG_BRIGHT_BLUE,
+        BG_BRIGHT_MAGENTA,
+        BG_BRIGHT_CYAN,
+        BG_BRIGHT_WHITE
+    };
+    
+    /**
      * @brief Returns a bolded string if ANSI is enabled
      */
     std::string bold(const std::string& str) {
